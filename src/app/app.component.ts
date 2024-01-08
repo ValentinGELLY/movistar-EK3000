dfaf7738b52073ecbc99f4efbf1202c83f0de182
ValentinGELLY <gelly.valentin@gmail.com>
2024-01-08 09:02:11 +0100

    init commit movistar$

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'monApplicationTestAngular2';
+  title = 'MOVISTAR-EK3000';
 }
